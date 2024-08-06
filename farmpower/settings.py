5ba519f9b97a7e3a943664aa5b8f2d3061322d35
Jibril <iam.olajire@gmail.com>
2024-08-07 00:49:59 +0100

    rem static

--- farmpower/settings.py
+++ farmpower/settings.py
@@ -115,11 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
 
 
 MEDIA_URL = "/media/"
