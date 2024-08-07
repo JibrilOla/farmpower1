0711d0634a9f229f7714a503443bd6441d7d329d
Jibril <iam.olajire@gmail.com>
2024-08-07 02:40:29 +0100

    cc

--- registration/management/commands/createsu.py
+++ registration/management/commands/createsu.py
@@ -6,9 +6,8 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='adminola',
-                password='Jibril120207'
+        User.objects.create_superuser(
+            username='adminnola',
+            password='Jibril120207'
             )
         print('Superuser has been created.')
